054737e1b36b54d8d625ba258a4903677eb51f18
Alaa Hassanein <hassaneinalaa@johndeere.com>
2020-03-17 14:56:30 -0500

    init commit

---
+++ src/main/java/demo/dronedemo/DronedemoApplication.java
@@ -0,0 +1,14 @@
+package demo.dronedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronedemoApplication.class, args);
+        System.out.println("test test");
+    }
+
+}
